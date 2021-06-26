9b24621d5b09c5d1a738364a380939912ce870a7
Junya0101 <21w4021e@shinshu-u.ac.jp>
2021-06-26 16:09:22 +0900

    効率的なフィボナッチ数列関数を作った

--- test02.py
+++ test02.py
@@ -1,7 +1,5 @@
 import time
 
-a = 200
-
 def fibo(n):
     if n == 0:
         return 1
@@ -9,18 +7,25 @@ def fibo(n):
         return 1
     return fibo(n - 1) + fibo(n - 2)
 
-a = a + a * 100
-print(a)
-
-print(a * 3)
-
-print("ばーか")
+def fibo2(n):
+    if n == 0:
+        return 1
+    if n == 1:
+        return 1
+    x1 = 1
+    x2 = 1
+    tmp = 0
+    for i in range(1, n):
+        tmp = x1 + x2
+        x1 = x2
+        x2 = tmp
+    return x2
 
 t0 = time.time()
 
-# print(fibo(30))
-# print(fibo(33))
-print(fibo(34))
-# print(fibo(40))
+n = 100
+
+# print(fibo(n))
+print(fibo2(n))
 
 print(time.time() - t0, "秒かかった")
