7aac27147d4e51e7a1a724ad34cb713e70bdc069
Junya0101 <21w4021e@shinshu-u.ac.jp>
2021-06-26 15:44:58 +0900

    フィボナッチ数列を作る関数作った

--- test02.py
+++ test02.py
@@ -1,7 +1,9 @@
-a = 1
-print(a)
+def fibo(n):
+    if n == 0:
+        return 1
+    if n == 1:
+        return 1
+    return fibo(n - 1) + fibo(n - 2)
 
-a = a + a * 100
-print(a)
-
-print(a * 3)
+for i in range(10):
+    print(fibo(i))
